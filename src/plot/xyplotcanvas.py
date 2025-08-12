EW and NS timeseries plots no longer use frequency thresholding. Fixed floating point comparision issues for certain frequency values. Removed unnecessary lines of code from the k-vector computation

src/plot/xyplotcanvas.py
@@ -20,23 +20,20 @@ class XYPlotCanvas(FigureCanvas):
         self.scatter_ew = None
         self.scatter_ns = None
         self.axs = [self.ax_range, self.ax_ew, self.ax_ns]
-        
-        self._set_plot_ax()
-        #self.plot_initial()
         self.is_hidden = False
         self.setHidden(self.is_hidden)
 
-    def _set_plot_ax(self):
+    def _set_plot_ax(self, site):
         self.ax_range.set_ylabel("Range (km)")
         self.ax_ns.set_ylabel("NS (km)")
         self.ax_ew.set_ylabel("EW (km)")
         for ax in self.axs:
-            ax.set_xlabel("Time")
+            ax.set_xlabel(f"Time in {site_dict[site].timezone}")
             date_format = mdates.DateFormatter("%H:%M")
             ax.xaxis.set_major_formatter(date_format)
             #ax.grid()
 
-    def plot_scatter(self, time_index, heights, x, y, xpow, output_freqs, selected_frequencies, date, site):
+    def plot_scatter(self, time_index, heights, freqs, x, y, xpow, output_freqs, selected_frequencies, date, site):
         for ax in self.axs:
             ax.clear()
         self.fig.tight_layout(pad=3)
@@ -46,21 +43,20 @@ class XYPlotCanvas(FigureCanvas):
         self.fig.suptitle(f"NS, EW, Range timeseries plot at site: {site} on {date.day:02d}-{date.month:02d}-{date.year:04d} {site_dict[site].timezone}")
         print(f"Selected frequencies: {selected_frequencies}")
         for freq in np.unique(selected_frequencies):
-            self.ax_range.plot(heights.iloc[np.argwhere(output_freqs == freq).flatten()], marker='s', linewidth=0, markersize=3, label=f"{freq/1e6} MHz")
-            self.ax_ew.plot(x.iloc[np.argwhere(output_freqs == freq).flatten()], linewidth=0, marker='s', markersize=3, label=f"{freq/1e6} MHz")
+            self.ax_range.plot(heights.iloc[np.argwhere(np.isclose(freqs, freq, atol=1e-12)).flatten()], marker='s', linewidth=0, markersize=3, label=f"{freq/1e6} MHz")
+            self.ax_ew.plot(x.iloc[np.argwhere(np.isclose(output_freqs, freq, atol=1e-12)).flatten()], linewidth=0, marker='s', markersize=3, label=f"{freq/1e6} MHz")
             
             self.ax_ew.set_ylim(-1000, 1000)
-            self.ax_ns.plot(y.iloc[np.argwhere(output_freqs == freq).flatten()], linewidth=0, marker='s', markersize=3, label=f"{freq/1e6} MHz")
+            self.ax_ns.plot(y.iloc[np.argwhere(np.isclose(output_freqs, freq, atol=1e-12)).flatten()], linewidth=0, marker='s', markersize=3, label=f"{freq/1e6} MHz")
             self.ax_ns.set_ylim(-1000, 1000)    
         for ax in self.axs:
             ax.set_xlim(time_index[0], time_index[-1])
-            ax.set_xlabel(f"Time {site_dict[site].timezone}")
             xaxis_timedelta = timedelta(hours=3) if len(np.unique(time_index)) > 72 else timedelta(hours=2) if len(np.unique(time_index)) > 36 else timedelta(minutes=30) if len(np.unique(time_index)) > 12 else timedelta(minutes=15)
             ax.set_xticks(np.arange(datetime(year=time_index[0].year, month=time_index[0].month, day=time_index[0].day, 
                                              hour=time_index[0].hour, minute=0, second=0), datetime(year=time_index[-1].year, month=time_index[-1].month, day=time_index[-1].day, 
                                              hour=time_index[-1].hour, minute=time_index[-1].minute, second=0) + timedelta(minutes=30), xaxis_timedelta))
             ax.margins(x=0,y=0)
-        self._set_plot_ax()
+        self._set_plot_ax(site)
         self._update_legend()
 
         self.fig.subplots_adjust(left=0.1, right=0.95, bottom=0.075, top=0.95)

src/utils/cadikvector.py
@@ -192,12 +192,8 @@ def compute_kvector(df, freq_list, sort_by_freq=False, points_thres=5):
         kx = kz * new_xy['x1']
         ky = kz * new_xy['x2']
         karray = np.vstack([kx, ky, kz]).T
-    if karray.shape[0] > 0:
-        df_tmp = pd.DataFrame({'kx': karray[:,0], 'ky': karray[:,1], 'kz': karray[:,2]}, index=timeindex_series)
-        k_out_with_ts = pd.concat([k_out_with_ts if not k_out_with_ts.empty else None, df_tmp])
-        all_output_heights = pd.concat([all_output_heights if not all_output_heights.empty else None, output_heights])
-        all_output_freqs = pd.concat([all_output_freqs if not all_output_freqs.empty else None, output_freqs])
-        all_output_pow = pd.concat([all_output_pow if not all_output_pow.empty else None, output_xpow])
+
+    k_out_with_ts = pd.DataFrame({'kx': karray[:,0], 'ky': karray[:,1], 'kz': karray[:,2]}, index=timeindex_series)
     return k_out_with_ts, output_freqs, output_heights, output_dops, output_signals, output_xpow
 
 def compute_vel(df, freq_list, points_thres=5):