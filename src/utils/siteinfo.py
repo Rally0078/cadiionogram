25535dcadc0d89752ab76000619721ea30e828a0
Jaideep Vimal Jothi <jaideepvimal.jothis@iigm.res.in>
2025-10-01 16:09:25 +0530

    fix: Changed gyrofrequencies and added TIFR Hyderabad station

--- src/utils/siteinfo.py
+++ src/utils/siteinfo.py
@@ -6,11 +6,14 @@ class SiteInfo():
     FH: float
     dip: float
     short_site: str
+#FH at 300 km altitude
+ald_site = SiteInfo(timezone='LT', FH=1.119, dip=10.2, short_site='al')
+tir_site = SiteInfo(timezone='UT', FH=0.951, dip=0.5, short_site='ti')
+hyd_site = SiteInfo(timezone='LT', FH=1.007, dip=6.5, short_site='tf')
 
-ald_site = SiteInfo(timezone='LT', FH=1.20, dip=10.2, short_site='al')
-tir_site = SiteInfo(timezone='UT', FH=1.03, dip=0.5, short_site='ti')
 site_dict = {
     'TIR': tir_site,
     'KSKGRL-IIGM PRAYAGRAJ': ald_site,
-    'ALD': ald_site
+    'ALD': ald_site,
+    'TFR': hyd_site
 }
