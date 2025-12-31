3d60af14fcc9887d7b36d64310edabb967ef7ba0
Jaideep Vimal Jothi <jaideepvimal.jothis@iigm.res.in>
2025-12-31 18:00:43 +0530

    Renamed old range-time-freq files and classes to new name

--- src/plotstate/mdx_rangetimefreq_state.py
+++ src/plotstate/mdx_rangetimefreq_state.py
@@ -1,16 +1,14 @@
 #Concrete implementation for range-time-freq plot
-from src.plot.xyplotcanvas import XYPlotCanvas
+from src.plot.rangetimefreqcanvas import RangeTimeFreqCanvas
 from src.plotstate.base import PlotState
-from datetime import datetime
 from src.utils.pandasutils import PandasUtils
-from src.utils.cadikvector import compute_xy
-from src.utils.powerpreprocessing import convert_amplitude_to_power
+from datetime import datetime
 from decimal import Decimal
 import pytz
 
-class MdxXYplotCanvasState(PlotState):
+class MdxRangeTimeFreqState(PlotState):
     def create_canvas(self):
-        canvas = XYPlotCanvas(self.main)
+        canvas = RangeTimeFreqCanvas(self.main)
         self.update_canvas(canvas)
         return canvas
 
@@ -30,20 +28,13 @@ class MdxXYplotCanvasState(PlotState):
         selected_frequencies = self.main.freq_selector.selectedItems()
         selected_frequencies_decimals = [Decimal(freq) for freq in selected_frequencies]
         selected_frequencies_rounded = [float(item.quantize(Decimal(f"1e-3"))) * 1e6 for item in selected_frequencies_decimals]
-
-        df_output, output_freqs, output_heights, output_dops, output_signals, output_xpow = compute_xy(df_selection, self.main.freqs_list, sort_by_freq=False)
         signal_col_names = [f"sensor{i//2 + 1} {'real' if i%2 == 0 else 'imag'}" for i in range(8)]
-        power = convert_amplitude_to_power(df_selection[signal_col_names].to_numpy())
+    
         canvas.plot_scatter(
             df_selection.index,
             df_selection['height (km)'],
-            df_selection['freq (Hz)'],
-            power,
-            df_output['xpos'],
-            df_output['ypos'], 
-            output_xpow,
-            output_freqs,
-            selected_frequencies_rounded,
+            df_selection['dopplershift'],
+            df_selection['freq (Hz)'], 
             self.main.metadata['datetime'],
             self.main.metadata['site']
         )
