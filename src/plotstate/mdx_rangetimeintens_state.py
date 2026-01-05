ce88058c89ca70b90c7f4fc795107cab5b77f95a
Jaideep Vimal Jothi <jaideepvimal.jothis@iigm.res.in>
2026-01-05 14:17:13 +0530

    Fixed timeseries plotting of MD4 format files

--- src/plotstate/mdx_rangetimeintens_state.py
+++ src/plotstate/mdx_rangetimeintens_state.py
@@ -2,6 +2,7 @@
 from src.plot.rangetimeintenscanvas import RangeTimeIntensCanvas
 from src.plotstate.base import PlotState
 from src.utils.powerpreprocessing import convert_amplitude_to_power
+from src.ionogramfiltering.noisereduction import o_x_separation
 from src.utils.pandasutils import PandasUtils
 from datetime import datetime
 from decimal import Decimal
@@ -26,18 +27,35 @@ class MdxRangeTimeIntensState(PlotState):
         start_dtime = start_dtime.replace(tzinfo=pytz.UTC)
         end_dtime = end_dtime.replace(tzinfo=pytz.utc)
         df_selection = df.loc[start_dtime:end_dtime]
-        selected_frequencies = self.main.freq_selector.selectedItems()
-        selected_frequencies_decimals = [Decimal(freq) for freq in selected_frequencies]
-        selected_frequencies_rounded = [float(item.quantize(Decimal(f"1e-3"))) * 1e6 for item in selected_frequencies_decimals]
         signal_col_names = [f"sensor{i//2 + 1} {'real' if i%2 == 0 else 'imag'}" for i in range(8)]
-        power = convert_amplitude_to_power(df_selection[signal_col_names].to_numpy())
-    
-        canvas.plot_scatter(
+        if self.main.md3_checkbox.isChecked():
+            selected_frequencies = self.main.freq_selector.selectedItems()
+            selected_frequencies_decimals = [Decimal(freq) for freq in selected_frequencies]
+            selected_frequencies_rounded = [float(item.quantize(Decimal(f"1e-3"))) * 1e6 for item in selected_frequencies_decimals]
+            power = convert_amplitude_to_power(df_selection[signal_col_names].to_numpy())
+            canvas.plot_scatter(
             df_selection.index,
             df_selection['height (km)'],
             power,
             df_selection['freq (Hz)'], 
             self.main.metadata['datetime'],
             self.main.metadata['site'],
+            True,
             selected_frequencies_rounded
         )
+        else:
+            selected_frequencies = self.main.freqs_list
+            selected_frequencies_rounded = selected_frequencies
+            freq_selection, height_selection, dop_selection, signals_selection = o_x_separation(df_selection['freq (Hz)'], 
+                           df_selection['height (km)'], 
+                           df_selection['dopplershift'],
+                           df_selection[signal_col_names].to_numpy())
+            canvas.plot_scatter(
+                freq_selection.index,
+                height_selection,
+                convert_amplitude_to_power(signals_selection),
+                freq_selection, 
+                self.main.metadata['datetime'],
+                self.main.metadata['site'],
+                False
+            )
